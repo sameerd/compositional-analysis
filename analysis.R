28c19f24410eba5cf0a871651ad8a7fcdcb0e652
Sameer DCosta <sameerdcosta@gmail.com>
2016-04-30 09:58:37 -0500

    Plotted the raw relative frequencies by time point.

--- analysis.R
+++ analysis.R
@@ -4,26 +4,60 @@ library(reshape2)
 # read in the csv data
 x <- read.csv("data/freqAnalOff_MAMcsfreqBinsMM_FINAL.csv")
 
+
+
 # Change add a character version of FreqBin
 x$FreqBinName <- paste0("FreqBin", x$FreqBin)
+x$Group <- factor(as.numeric(x$Group), levels=c(0, 1), labels=c("A", "B"))
 
 # reshape the data so that it is wide instead of long
 x.reshape <- dcast(x, id+TimePt+Group~FreqBinName, value.var="emg50")
 
-# Extract userful columns
-dat.useful <- x.reshape[, c("Group", paste0("FreqBin", 1:4))]
+# verify that the frequencies add up to 1
+freqsums <- x.reshape$FreqBin1 + x.reshape$FreqBin2 + x.reshape$FreqBin3 + x.reshape$FreqBin4
+# plot(freqsums)
 
-# Split data into treatment and control groups
-dat.split <- split(dat.useful, dat.useful$Group)
+# split all the data by TimePt
+x.split <- split(x.reshape, x.reshape$TimePt)
 
-dat.group0 <- dat.split[[1]]
-dat.group0$Group <- NULL
+# we are making an assumption here that all the patients are in the same order 
+# on each split. No way to verify this given the data because there is no patient 
+# id but but the group orders look like they match
+all.equal(x.split[[1]]$Group, x.split[[2]]$Group)
+all.equal(x.split[[1]]$Group, x.split[[3]]$Group)
+all.equal(x.split[[1]]$Group, x.split[[4]]$Group)
+all.equal(x.split[[1]]$Group, x.split[[5]]$Group)
 
-dat.group1 <- dat.split[[2]]
-dat.group1$Group <- NULL
+# Lets make this explicit by making the id equal to the patient id
+# FIXME: VERIFY THIS FROM THE ORIGINAL DATA
+for(i in 2:5) {
+  x.split[[i]]$id <- x.split[[1]]$id
+}
 
-# apply relative compositions
-dat0.rcomp <- rcomp(dat.group0)
-dat1.rcomp <- rcomp(dat.group1)
+# data to analyze in wide format with 4 bins
+x4 <- x.split
 
+# Combine bins 1 and 2 so that we have data with 3 bins
+# Rename the combined bin FreqBin12
+x3 <- lapply(x4, function(z) {
+  z$FreqBin1 <- z$FreqBin1 + z$FreqBin2
+  z$FreqBin2 <- NULL
+  names(z) <- sub("FreqBin1", "FreqBin12", names(z))
+  z
+})
 
+# now further split x3 and x4 by group
+x4 <- lapply(x4, function(z) split(z, z$Group))
+x3 <- lapply(x3, function(z) split(z, z$Group))
+
+# Function to just extract columns named FreqBin
+ExtractFreqBins <- function(z.df) z.df[, grep("FreqBin", names(z.df))]
+
+# Plot the vectors (now the ones that dont add up to one will be forced to add up to one)
+# These are not the effects you seek
+op <- par(mfrow=c(3,2))
+for (i in 1:5) {
+  plot(rcomp(ExtractFreqBins(x3[[5]][[1]])), main=paste("Time point ", i ))
+  plot(rcomp(ExtractFreqBins(x3[[5]][[2]])), add=TRUE, col="red")
+}
+par(op)
