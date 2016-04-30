34bf74c24b798bab463a1cb4a0007d0ad8f443ca
Sameer DCosta <sameerdcosta@gmail.com>
2016-04-30 08:53:11 -0500

    Added documentation

--- analysis.R
+++ analysis.R
@@ -1,12 +1,19 @@
 library(compositions)
 library(reshape2)
 
+# read in the csv data
 x <- read.csv("data/freqAnalOff_MAMcsfreqBinsMM_FINAL.csv")
-x$FreqBinName <- paste0("FreqBin", x$FreqBin)
-x.reshape <- dcast(x, id+TimePt+Group~FreqBinName, value.var="emg50")
 
+# Change add a character version of FreqBin
+x$FreqBinName <- paste0("FreqBin", x$FreqBin)
+
+# reshape the data so that it is wide instead of long
+x.reshape <- dcast(x, id+TimePt+Group~FreqBinName, value.var="emg50Cs")
+
+# Extract userful columns
 dat.useful <- x.reshape[, c("Group", paste0("FreqBin", 1:4))]
 
+# Split data into treatment and control groups
 dat.split <- split(dat.useful, dat.useful$Group)
 
 dat.group0 <- dat.split[[1]]
@@ -15,6 +22,7 @@ dat.group0$Group <- NULL
 dat.group1 <- dat.split[[2]]
 dat.group1$Group <- NULL
 
+# apply relative compositions
 dat0.rcomp <- rcomp(dat.group0)
 dat1.rcomp <- rcomp(dat.group1)
 
